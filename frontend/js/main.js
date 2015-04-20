4b182482ad0b4052bbc06bacf013d59694dfeb87
andrewchosen <andrew.chosen@gmail.com>
2015-04-20 15:46:03 -0700

    various changes

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,4 +3,14 @@ $(document).ready(function(){
 	$('.search-button').click(function(){
 		$('.search-box').toggle('fast');
 	});
+
+	$('.quantity').keyup(function() {
+	   var quantity = $(".quantity input").text();
+	   var price = $(".price .value").val();
+
+	   var total = quantity * price;
+
+	   $("#total_price").val(total); // sets the total price input to the quantity * price
+	});
+
 });
